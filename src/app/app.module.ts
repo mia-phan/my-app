d20ba4562ef0af231f2621ee12c8f7bbcaa85662
zelhel <miaphan.pma@gmail.com>
2023-05-25 03:32:42 +0000

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ProjectComponent } from './project/project.component';
+
+const routes: Routes =[
+  { path: '', component: HomeComponent },
+  { path: 'projects', component: ProjectComponent },
+  { path: 'about', component: AboutMeComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavigationComponent
+    NavigationComponent,
+    AboutMeComponent,
+    ProjectComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
