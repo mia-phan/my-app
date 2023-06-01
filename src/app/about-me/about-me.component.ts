f289f0b6e572a12814a97a5907a20c95d1909082
miaphan <69333723+mia-phan@users.noreply.github.com>
2023-06-01 14:46:54 +1200

    add cloudinary integration (#12)

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CloudinaryImage } from '@cloudinary/url-gen';
 
 @Component({
   selector: 'app-about-me',
@@ -6,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-me.component.css']
 })
 export class AboutMeComponent implements OnInit {
-
+  myImage =  new CloudinaryImage('v1685585511/my-app/2848BF11-215B-4AA7-827A-5671DEE52AB1_akvk6s', {cloudName: 'depf1ep80'});
   constructor() { }
-
+  
   ngOnInit(): void {
   }
 
