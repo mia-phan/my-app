96cce453efd5e89ede9d409d51d66fbc712063f2
miaphan <69333723+mia-phan@users.noreply.github.com>
2023-08-11 16:28:48 +1200

    fix (#25)

--- src/app/project/project-illustration/illustration-dialog/illustration-dialog.module.ts
+++ src/app/project/project-illustration/illustration-dialog/illustration-dialog.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IllustrationDialogComponent } from './illustration-dialog.component';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [IllustrationDialogComponent],
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, MatIconModule, MatDialogModule],
   exports: [IllustrationDialogComponent],
 })
 export class IllustrationDialogModule {}
