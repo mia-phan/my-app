5a183670657c7aa30b6b584f50335440821eb838
zelhel <miaphan.pma@gmail.com>
2023-08-28 06:17:30 +0000

    responsive navbar

--- src/app/navigation/navigation.module.ts
+++ src/app/navigation/navigation.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+
 import { NavigationComponent } from './navigation.component';
 
 @NgModule({
   declarations: [NavigationComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatButtonModule, MatMenuModule],
   exports: [NavigationComponent],
 })
 export class NavigationModule {}
