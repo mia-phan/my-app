929d059c62a777564254cd0e4ef2ec449a78faa0
zelhel <miaphan.pma@gmail.com>
2023-09-01 04:07:24 +0000

    wip

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+
 import { HeaderModule } from '../utilities/header/header.module';
 import { HomeComponent } from './home.component';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HeaderModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    HeaderModule,
+  ],
   exports: [HomeComponent],
 })
 export class HomeModule {}
