0c57c8c926ae8a37a550b55051b1a166712f5813
miaphan <69333723+mia-phan@users.noreply.github.com>
2023-09-08 18:58:06 +1200

    fix: image-dialog (#35)

--- src/app/project/project-illustration/illustration-dialog/illustration-dialog.module.ts
+++ src/app/project/project-illustration/illustration-dialog/illustration-dialog.module.ts
@@ -5,8 +5,9 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-
+import { MatInputModule } from '@angular/material/input';
 import { ShareBottomSheetModule } from './share-bottom-sheet/share-bottom-sheet.module';
+
 import { IllustrationDialogComponent } from './illustration-dialog.component';
 
 @NgModule({
@@ -17,6 +18,7 @@ import { IllustrationDialogComponent } from './illustration-dialog.component';
     MatDialogModule,
     MatBottomSheetModule,
     MatSnackBarModule,
+    MatInputModule,
     ShareBottomSheetModule,
   ],
 })
